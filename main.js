361e613e100996a650350b0856be787341853e0d
Lucas Rocha <49926164+futuruz-rt@users.noreply.github.com>
2021-03-14 17:47:29 -0300

    Uso das informações da API no arquivo index.html

--- main.js
+++ main.js
@@ -2,18 +2,95 @@ $(document).ready(function(){
     $("#consulta").on('click' ,async function(){
         const cidade = $('input[name="cidade"]').val();
         const apiKey = '0a5ea4492af408582dd019a84a1e5085';
-        const respostaReq = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cidade + "&units=metric&lang=pt-br&appid=" + apiKey);
+        const respostaReq = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cidade + "&units=metric&lang=pt&appid=" + apiKey);
         const dados = await respostaReq.json();
-		
-        console.log(dados);
-        console.log(dados.weather[0].description);
-        console.log(dados.main.temp);
-        console.log(dados.main.feels_like);
-        console.log(dados.main.humidity);	
+	    
+	switch(dados.weather[0].icon){
+            case '01d':
+                $("#tempoMarca").html("<img src='http://openweathermap.org/img/wn/01d@2x.png'>");
+                break;
+
+            case '02d':
+                $("#tempoMarca").html("<img src='http://openweathermap.org/img/wn/02d@2x.png'>");
+                break;
+
+            case '03d':
+                $("#tempoMarca").html("<img src='http://openweathermap.org/img/wn/03d@2x.png'>");
+                break;
+
+            case '04d':
+                $("#tempoMarca").html("<img src='http://openweathermap.org/img/wn/04d@2x.png'>");
+                break;
+
+            case '09d':
+                $("#tempoMarca").html("<img src='http://openweathermap.org/img/wn/09d@2x.png'>");
+                break;
+
+            case '10d':
+                $("#tempoMarca").html("<img src='http://openweathermap.org/img/wn/10d@2x.png'>");
+                break;
+
+            case '11d':
+                $("#tempoMarca").html("<img src='http://openweathermap.org/img/wn/11d@2x.png'>");
+                break;
+
+            case '13d':
+                $("#tempoMarca").html("<img src='http://openweathermap.org/img/wn/13d@2x.png'>");
+                break;
+
+            case '50d':
+                $("#tempoMarca").html("<img src='http://openweathermap.org/img/wn/50d@2x.png'>");
+                break;
+			
+            case '01n':
+                $("#tempoMarca").html("<img src='http://openweathermap.org/img/wn/01n@2x.png'>");
+                break;
+
+            case '02n':
+                $("#tempoMarca").html("<img src='http://openweathermap.org/img/wn/02n@2x.png'>");
+                break;
+
+            case '03n':
+                $("#tempoMarca").html("<img src='http://openweathermap.org/img/wn/03n@2x.png'>");
+                break;
+
+            case '04n':
+                $("#tempoMarca").html("<img src='http://openweathermap.org/img/wn/04n@2x.png'>");
+                break;
+
+            case '09n':
+                $("#tempoMarca").html("<img src='http://openweathermap.org/img/wn/09n@2x.png'>");
+                break;
+
+            case '10n':
+                $("#tempoMarca").html("<img src='http://openweathermap.org/img/wn/10n@2x.png'>");
+                break;
+
+            case '11n':
+                $("#tempoMarca").html("<img src='http://openweathermap.org/img/wn/11n@2x.png'>");
+                break;
+
+            case '13n':
+                $("#tempoMarca").html("<img src='http://openweathermap.org/img/wn/13n@2x.png'>");
+                break;
+
+            case '50n':
+                $("#tempoMarca").html("<img src='http://openweathermap.org/img/wn/50n@2x.png'>");
+                break;
+
+        }
+
+        $("#tempo").html(dados.weather[0].description);
+        $("#temp").html(dados.main.temp);
+		$("#umidade").html(dados.main.humidity);       
     })
 
     $("#limpar").on('click' ,function(){
         $('#cidade').val('');
+		$('#tempo').empty();
+        $('#temp').empty();
+        $('#umidade').empty();
+        $('#tempoMarca').empty();
     })       
        
 })
